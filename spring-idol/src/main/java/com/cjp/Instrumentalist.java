05d4fb87c944c808760976a0eb1e4be07c9d7b27
JiapengChen233 <m15766950919@163.com>
2021-05-29 10:39:11 +0800

    使用注解装配

--- spring-idol/src/main/java/com/cjp/Instrumentalist.java
+++ spring-idol/src/main/java/com/cjp/Instrumentalist.java
@@ -1,7 +1,12 @@
 package com.cjp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Instrumentalist implements Performer {
     private String song;
+    @Autowired
+    @Qualifier("guitar")
     private Instrument instrument;
 
     @Override
---
+++ spring-idol/src/test/java/com/cjp/SpringContextTest3.java
@@ -0,0 +1,28 @@
+package com.cjp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:spring-idol-context3.xml"
+})
+public class SpringContextTest3 {
+
+    @Autowired
+    private ApplicationContext ctx;
+
+    @Test
+    public void autowireByAnnotation() {
+        Performer kenny = (Performer) ctx.getBean("kenny");
+        assertNotNull(kenny);
+        kenny.perform();
+    }
+
+}
