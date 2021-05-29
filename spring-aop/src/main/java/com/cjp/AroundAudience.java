e5d5b1b72ad2a905436920bf8b8c03281ac5736d
JiapengChen233 <m15766950919@163.com>
2021-05-29 13:47:29 +0800

    声明环绕通知

---
+++ spring-aop/src/main/java/com/cjp/AroundAudience.java
@@ -0,0 +1,20 @@
+package com.cjp;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class AroundAudience {
+
+    public void watchPerformance(ProceedingJoinPoint joinPoint) {
+        try {
+            System.out.println("The audience is taking their seats.");
+            System.out.println("The audience is turning off their cellphones");
+            long start = System.currentTimeMillis();
+            joinPoint.proceed();
+            long end = System.currentTimeMillis();
+            System.out.println("CLAP CLAP CLAP CLAP");
+            System.out.println("The performance took " + (end - start) + " milliseconds.");
+        } catch (Throwable t) {
+            System.out.println("Boo! We want our money back!");
+        }
+    }
+}
