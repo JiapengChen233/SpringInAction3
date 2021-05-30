5212559d4dbf84ca5a45f001815c5f1a1c289b65
JiapengChen233 <m15766950919@163.com>
2021-05-30 10:54:39 +0800

    使用DBCP配置内置数据源

---
+++ Spitter/spitter-persistence-common/src/test/java/com/cjp/spitter/persistence/HsqldbTest.java
@@ -0,0 +1,34 @@
+package com.cjp.spitter.persistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:test-dataSource-context.xml"
+})
+public class HsqldbTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from spitter");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            String articleName = resultSet.getString("username");
+            System.out.println("username: " + articleName);
+        }
+    }
+}
