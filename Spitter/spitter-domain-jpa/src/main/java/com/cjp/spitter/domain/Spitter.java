05369d95a5eccd47f81966eed32672e5465dc24f
JiapengChen233 <m15766950919@163.com>
2021-06-05 23:36:28 +0800

    校验表单输入

--- Spitter/spitter-domain-jpa/src/main/java/com/cjp/spitter/domain/Spitter.java
+++ Spitter/spitter-domain-jpa/src/main/java/com/cjp/spitter/domain/Spitter.java
@@ -1,6 +1,8 @@
 package com.cjp.spitter.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -10,9 +12,18 @@ public class Spitter implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
+
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters long.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Username must be alphanumeric with no spaces")
     private String username;
+
+    @Size(min = 6, max = 20, message = "The password must be at least 6 characters long.")
     private String password;
+
+    @Size(min = 3, max = 50, message = "Your full name must be between 3 and 50 characters long.")
     private String fullName;
+
+    @Pattern(regexp = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}", message = "Invalid email address.")
     private String email;
     private boolean updateByEmail;
 
--- Spitter/spitter-web/src/main/java/com/cjp/spitter/mvc/SpitterController.java
+++ Spitter/spitter-web/src/main/java/com/cjp/spitter/mvc/SpitterController.java
@@ -6,10 +6,12 @@ import com.cjp.spitter.service.SpitterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -40,7 +42,10 @@ public class SpitterController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String addSpitterFromForm(Spitter spitter) {
+    public String addSpitterFromForm(@Valid Spitter spitter, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "spitters/edit";
+        }
         spitterService.saveSpitter(spitter);
         return "redirect:/spitters/" + spitter.getUsername();
     }
