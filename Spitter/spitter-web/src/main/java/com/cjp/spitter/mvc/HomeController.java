77689bfbedd48c2eca648296e78a897a83d1163c
RonaldoC <m15766950919@163.com>
2021-06-18 16:48:21 +0800

    外部化配置

--- Spitter/spitter-web/src/main/java/com/cjp/spitter/mvc/HomeController.java
+++ Spitter/spitter-web/src/main/java/com/cjp/spitter/mvc/HomeController.java
@@ -1,26 +1,17 @@
 package com.cjp.spitter.mvc;
 
-import com.cjp.spitter.jmx.HomeControllerManagedOperations;
 import com.cjp.spitter.service.SpitterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jmx.export.MBeanExporter;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedResource;
-import org.springframework.jmx.export.assembler.InterfaceBasedMBeanInfoAssembler;
-import org.springframework.jmx.export.assembler.MBeanInfoAssembler;
-import org.springframework.jmx.support.ConnectorServerFactoryBean;
-import org.springframework.jmx.support.MBeanRegistrationSupport;
-import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
-//@ManagedResource(objectName="spitter:name=HomeController") //将HomeController导出为MBean
+@ManagedResource(objectName="spitter:name=HomeController") //将HomeController导出为MBean
 public class HomeController {
 
     public static final int DEFAULT_SPITTLES_PER_PAGE = 25;
@@ -37,13 +28,13 @@ public class HomeController {
     }
 
     //@ManagedOperation@ManagedOperation注解替换@ManagedAttribute注解来标注存取器方法
-//    @ManagedAttribute        //将spittlesPerPage暴露为托管属性
+    @ManagedAttribute        //将spittlesPerPage暴露为托管属性
     public int getSpittlesPerPage() {
         return spittlesPerPage;
     }
 
     //@ManagedOperation@ManagedOperation注解替换@ManagedAttribute注解来标注存取器方法
-//    @ManagedAttribute        //将spittlesPerPage暴露为托管属性
+    @ManagedAttribute        //将spittlesPerPage暴露为托管属性
     public void setSpittlesPerPage(int spittlesPerPage) {
         this.spittlesPerPage = spittlesPerPage;
     }
@@ -57,37 +48,22 @@ public class HomeController {
 //        return assembler;
 //    }
 
-    @Bean
-    public InterfaceBasedMBeanInfoAssembler assembler() {
-        InterfaceBasedMBeanInfoAssembler assembler = new InterfaceBasedMBeanInfoAssembler();
-        assembler.setManagedInterfaces(new Class[]{HomeControllerManagedOperations.class});
-        return assembler;
-    }
+//    @Bean
+//    public InterfaceBasedMBeanInfoAssembler assembler() {
+//        InterfaceBasedMBeanInfoAssembler assembler = new InterfaceBasedMBeanInfoAssembler();
+//        assembler.setManagedInterfaces(new Class[]{HomeControllerManagedOperations.class});
+//        return assembler;
+//    }
+//
+//    @Bean
+//    public MBeanExporter mbeanExporter(HomeController homeController, MBeanInfoAssembler assembler) {
+//        MBeanExporter exporter = new MBeanExporter();
+//        Map<String, Object> beans = new HashMap<String, Object>();
+//        beans.put("spitter:name=HomeController", homeController);
+//        exporter.setBeans(beans);
+//        exporter.setAssembler(assembler);
+//        exporter.setRegistrationBehavior(MBeanRegistrationSupport.REGISTRATION_IGNORE_EXISTING);
+//        return exporter;
+//    }
 
-    @Bean
-    public MBeanExporter mbeanExporter(HomeController homeController, MBeanInfoAssembler assembler) {
-        MBeanExporter exporter = new MBeanExporter();
-        Map<String, Object> beans = new HashMap<String, Object>();
-        beans.put("spitter:name=HomeController", homeController);
-        exporter.setBeans(beans);
-        exporter.setAssembler(assembler);
-        exporter.setRegistrationBehavior(MBeanRegistrationSupport.REGISTRATION_IGNORE_EXISTING);
-        return exporter;
-    }
-
-    //启动一个RMI注册表，并监听1099端口
-    @Bean
-    public RmiRegistryFactoryBean rmiRegistryFB() {
-        RmiRegistryFactoryBean rmiRegistryFB = new RmiRegistryFactoryBean();
-        rmiRegistryFB.setPort(1099);
-        return rmiRegistryFB;
-    }
-
-    //将ConnectorServerFactoryBean绑定到了上面的RMI注册表，该注册表监听本机的1099端口
-    @Bean
-    public ConnectorServerFactoryBean connectorServerFactoryBean() {
-        ConnectorServerFactoryBean csfb = new ConnectorServerFactoryBean();
-        csfb.setServiceUrl("service:jmx:rmi://localhost:1099/jndi/rmi://localhost:1099/spitter");
-        return csfb;
-    }
 }
